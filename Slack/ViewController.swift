296e63ae9ad14d15851e5195188a91e6dac664cc
Suzy Kim <suzy.kim@anbioninc.com>
2015-07-08 10:26:34 -0700

    Initial Commit

---
+++ Slack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Slack
+//
+//  Created by Suzy Cantabile on 7/8/15.
+//  Copyright (c) 2015 Suzy Cantabile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
